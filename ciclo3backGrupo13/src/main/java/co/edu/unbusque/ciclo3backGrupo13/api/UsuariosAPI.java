74552f19446b2360fe9eeec15a1c6b19d1280648
Harold Gomez <xagobo14@gmail.com>
2021-09-28 13:56:56 -0500

    actualizacion

--- ciclo3backGrupo13/src/main/java/co/edu/unbusque/ciclo3backGrupo13/api/UsuariosAPI.java
+++ ciclo3backGrupo13/src/main/java/co/edu/unbusque/ciclo3backGrupo13/api/UsuariosAPI.java
@@ -1,7 +1,6 @@
 package co.edu.unbusque.ciclo3backGrupo13.api;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
